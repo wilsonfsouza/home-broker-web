094dd7a5adb0c76ed6f3e3e36c4a3ffe90cc0fbe
Wilson Franca <wilson.franca.92@gmail.com>
2024-01-29 13:43:39 -0800

    feat(order-form): added custom error message for invalid types for shares and price

--- src/components/order-form/form-schema.ts
+++ src/components/order-form/form-schema.ts
@@ -5,12 +5,14 @@ export const formSchema = z.object({
   wallet_id: z.string(),
   type: z.enum(["BUY", "SELL"]),
   shares: z.number({
-    required_error: "You need at least 1 share to perform a new transaction"
+    required_error: "You need at least 1 share to perform a new transaction",
+    invalid_type_error: "Amount must be a number"
   }).min(1, {
     message: "You need at least 1 share to perform a new transaction"
   }),
   price: z.number({
-    required_error: "Price per stock share must be at least $1"
+    required_error: "Price per stock share must be at least $1",
+    invalid_type_error: "Price must be a number"
   }).min(1, {
     message: "Price per stock share must be at least $1"
   }),
